f25253c7f08f7dc468813bf37275335dcfbba605
kazuyafaizad <kazuyafaizad@gmail.com>
2022-05-09 01:28:00 +0800

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['"Bodoni Moda"'],
+      html: ['"Bodoni Moda"'],
+    },
+    extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
